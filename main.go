da1462d56016533dd158f2004804b30ca3ab0ea1
Shri Om Trivedi <tshriom@gmail.com>
2024-12-15 13:04:57 +0530

    structs go

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import "fmt"
+
+type contact struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contact
+}
+
 func main() {
-	
+	var name person
+	name.firstName = "John"
+	name.lastName = "Doe"
+	name.contact.email = "John@doe.com"
+	name.contact.zipCode = 233434
+
+	fmt.Println(name)
 }
